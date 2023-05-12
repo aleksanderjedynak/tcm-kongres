b88ea11dcfb1d9d9264199e0aa24546d264b7a2e
Aleksander Jedynak <aleksander.jedynak@softax.pl>
2023-05-12 13:48:17 +0200

    Organizers: zmiana widoku

--- src/components/Organizers.js
+++ src/components/Organizers.js
@@ -1,62 +1,115 @@
-const features = [
-  {
-    name: "Polskie Towarzystwo Tradycyjnej Medycyny Chińskiej",
-    description: "www.pttmc.org",
-    link: "www.pttmc.org",
-  },
-  {
-    name: "Instytut Medycyny Chińskiej i Profilaktyki Zdrowia",
-    description: "www.medycyna-chinska.com",
-  },
-  {
-    name: "Centrum Medycyny Chińskiej, Kraków",
-    description: "www.medycynachinska.org",
-  },
-  {
-    name: "TCM Academy of Chinese Medicine",
-    description: "www.tcm.ac",
-  },
-];
-
-//todo: poprawa gradientu
-
 export default function Organizers() {
   return (
-    <div className="bg-white">
+    <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Organizatorzy
-          </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Organizatorami wydarzenia są eksperci z dziedziny medycyny chińskiej
-            oraz lokalne instytucje naukowe z Krakowa. Ich misją jest
-            propagowanie wiedzy o medycynie chińskiej oraz umożliwienie wymiany
-            doświadczeń pomiędzy specjalistami z Polski i zagranicy. Dzięki ich
-            zaangażowaniu, uczestnicy będą mieli okazję uczestniczyć w
-            inspirujących wykładach, warsztatach oraz dyskusjach, które pozwolą
-            na zdobycie praktycznej wiedzy oraz nawiązanie cennych kontaktów w
-            branży.
-          </p>
-        </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-16  text-center">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  {feature.name}
-                </dt>
-                <a
-                  href={feature.description}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  {feature.description}
-                </a>
+        <div className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+            <div>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+                Organizatorzy
+              </h2>
+              <p className="mt-4 leading-7 text-gray-600">
+                Organizatorami wydarzenia są eksperci z dziedziny medycyny
+                chińskiej oraz lokalne instytucje naukowe z Krakowa. Ich misją
+                jest propagowanie wiedzy o medycynie chińskiej oraz umożliwienie
+                wymiany doświadczeń pomiędzy specjalistami z Polski i zagranicy.
+                Dzięki ich zaangażowaniu, uczestnicy będą mieli okazję
+                uczestniczyć w inspirujących wykładach, warsztatach oraz
+                dyskusjach, które pozwolą na zdobycie praktycznej wiedzy oraz
+                nawiązanie cennych kontaktów w branży.
+              </p>
+            </div>
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+              <div className="rounded-2xl bg-gray-50 p-10">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  Polskie Towarzystwo Tradycyjnej Medycyny Chińskiej
+                </h3>
+                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                  <div>
+                    <dt className="sr-only">Strona www</dt>
+                    <dd>
+                      <a
+                        className="font-semibold text-indigo-600"
+                        href="www.pttmc.org"
+                      >
+                        www.pttmc.org
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="mt-1">
+                    <dt className="sr-only">Number</dt>
+                    <dd>+48 111 111 111</dd>
+                  </div>
+                </dl>
               </div>
-            ))}
-          </dl>
+              <div className="rounded-2xl bg-gray-50 p-10">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  Instytut Medycyny Chińskiej i Profilaktyki Zdrowia
+                </h3>
+                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                  <div>
+                    <dt className="sr-only">Strona www</dt>
+                    <dd>
+                      <a
+                        className="font-semibold text-indigo-600"
+                        href="www.medycyna-chinska.com"
+                      >
+                        www.medycyna-chinska.com
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="mt-1">
+                    <dt className="sr-only">Number</dt>
+                    <dd>+48 111 111 111</dd>
+                  </div>
+                </dl>
+              </div>
+              <div className="rounded-2xl bg-gray-50 p-10">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  Centrum Medycyny Chińskiej, Kraków
+                </h3>
+                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                  <div>
+                    <dt className="sr-only">Strona www</dt>
+                    <dd>
+                      <a
+                        className="font-semibold text-indigo-600"
+                        href="www.medycynachinska.org"
+                      >
+                        www.medycynachinska.org
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="mt-1">
+                    <dt className="sr-only">Number</dt>
+                    <dd>+48 111 111 111</dd>
+                  </div>
+                </dl>
+              </div>
+              <div className="rounded-2xl bg-gray-50 p-10">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  TCM Academy of Chinese Medicine
+                </h3>
+                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                  <div>
+                    <dt className="sr-only">Strona www</dt>
+                    <dd>
+                      <a
+                        className="font-semibold text-indigo-600"
+                        href="www.tcm.ac"
+                      >
+                        www.tcm.ac
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="mt-1">
+                    <dt className="sr-only">Number</dt>
+                    <dd>+48 111 111 111</dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
